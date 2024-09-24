3723ccd01a7ea26a5098991c87e73c7d15d746b0
Carlos V <faotqm@hotmail.com>
2024-09-24 09:44:52 -0600

    se implementa el mailer

--- src/controllers/controllers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.controllers = void 0;
-var entregas_controller_1 = require("./entregas.controller");
-var user_controller_user_1 = require("./user.controller.user");
-exports.controllers = {
-    userController: new user_controller_user_1.default(),
-    entregasController: new entregas_controller_1.default
-};
